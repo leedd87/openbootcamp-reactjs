ab637ba237cc298f876c93a1aa7f512557d72ce7
leedd87 <leedav87@gmail.com>
2022-08-31 16:01:35 -0300

    importacion de componentes

--- src/components/pure/Greeting.jsx
+++ src/components/pure/Greeting.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+//React Class component
+import React, { Component } from "react";
+import PropTypes from "prop-types";
 
-const Greeting = () => {
-	return <div>Greeting</div>;
-};
+class Greeting extends Component {
+	render() {
+		return (
+			<div>
+				<h1>Hola!</h1>
+			</div>
+		);
+	}
+}
+
+Greeting.propTypes = {};
 
 export default Greeting;
+
+// import React from "react";
+
+// const Greeting = () => {
+// 	return <div>Greeting</div>;
+// };
+
+// export default Greeting;

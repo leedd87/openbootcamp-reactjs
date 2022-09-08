7112d29b4a65a479560d12d50f62287753554a98
leedd87 <leedav87@gmail.com>
2022-09-07 22:50:33 -0300

    hooks

--- src/components/pure/GreetingF.jsx
+++ src/components/pure/GreetingF.jsx
@@ -10,7 +10,7 @@ const GreetingF = (props) => {
 	return (
 		<div>
 			<h1>Hola David desde componente funcional!</h1>
-			<h2>Tu edad es de: {age}</h2>
+			<h2>Tu edad es de: {age} anios</h2>
 			<div>
 				<button onClick={birthday}>Cumplir anios</button>
 			</div>

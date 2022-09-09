071bba4757698ac2e5e702572ca23e9f7408a26e
leedd87 <leedav87@gmail.com>
2022-09-08 21:44:57 -0300

    sesion 3 completed

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from "./logo.svg";
 import "./App.css";
 // import Ejemplo1 from "./hooks/Ejemplo1";
 import Ejemplo2 from "./hooks/Ejemplo2";
+import Ejemplo4 from "./hooks/Ejemplo4";
 //import Greeting from "./components/pure/Greeting";
 // import GreetingF from "./components/pure/GreetingF";
 // import TaskListComponent from "./components/container/TaskListComponent";
@@ -16,7 +17,11 @@ function App() {
 				{/* <TaskListComponent /> */}
 				{/* <ContactContainerComponent /> */}
 				{/* <Ejemplo1 /> */}
-				<Ejemplo2 />
+				{/* <Ejemplo2 /> */}
+				<Ejemplo4 nombre="David">
+					{/* Todo lo que hay aqui, es tratado como un props.children */}
+					<h3>Contenido del props.children</h3>
+				</Ejemplo4>
 			</header>
 		</div>
 	);
---
+++ src/hooks/Ejemplo4.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Ejemplo4 = (props) => {
+	return (
+		<div>
+			<h1>****Ejemplo de CHILDREN PROPS****</h1>
+			<h2>Nombre: {props.nombre}</h2>
+			{/*props.children pintara por defecto aquello que se encuentre entre las etiquetas de apertura y cierra de este componente desde componente de orden superior */}
+			{props.children}
+		</div>
+	);
+};
+
+export default Ejemplo4;

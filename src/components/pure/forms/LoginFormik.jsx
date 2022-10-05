b2d77ed10e6527031da408c398b04bd4f71e4422
leedd87 <leedav87@gmail.com>
2022-10-05 16:14:57 -0300

    basic formik

---
+++ src/components/pure/forms/LoginFormik.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { Formik, Field, Form } from "formik";
+
+const LoginFormik = () => {
+	const initialCredentials = {
+		email: "",
+		password: "",
+	};
+
+	return (
+		<div>
+			<h4>Login Formik</h4>
+			<Formik
+				initialValues={initialCredentials}
+				onSubmit={async (values) => {
+					await new Promise((r) => setTimeout(r, 500));
+					alert(JSON.stringify(values, null, 2));
+				}}
+			>
+				<Form>
+					<label htmlFor="email">Email Address</label>
+					<Field
+						id="email"
+						type="email"
+						name="email"
+						placeholder="example@email.com"
+					/>
+					<label htmlFor="password">Email Address</label>
+					<Field
+						id="password"
+						type="password"
+						name="password"
+						placeholder="password"
+					/>
+					<button type="submit">Login</button>
+				</Form>
+			</Formik>
+			;
+		</div>
+	);
+};
+
+export default LoginFormik;

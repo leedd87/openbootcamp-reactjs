2781429d566920f6bd11fb50aab9d752523a1194
leedd87 <leedav87@gmail.com>
2022-10-13 23:00:51 -0300

    openbootcamp

--- src/components/pure/forms/LoginFormik.jsx
+++ src/components/pure/forms/LoginFormik.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Formik, Field, Form } from "formik";
-import * as Yup from "yup";
+//import * as Yup from "yup";
 
 const LoginFormik = () => {
 	const initialCredentials = {
